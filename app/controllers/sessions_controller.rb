e80cc3bc4ff9aa20caba1dabd47f54c38d56c9f2
Benshalom <benshalom.kariuki@kopokopo.com>
2022-02-02 22:08:56 +0300

    restrict views at controller layer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
-        
+      before_action :logged_in_redirect, only: [:new, :create]  
+
+
 def new
 
 end 
@@ -23,5 +25,14 @@ session[:user_id] = nil
 flash[:success] = "Successfully logged out"
 redirect_to login_path
 end
+
+private 
+def logged_in_redirect
+if logged_in?
+    flash[:error] = "You are already signed in"
+redirect_to root_path
+end 
+
+end 
 
 end
